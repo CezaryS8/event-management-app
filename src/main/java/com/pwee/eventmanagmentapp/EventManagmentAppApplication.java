40732ec3b9869aacedb65182a911c34f9a013599
cezarys8 <czarek.skorupski2001@gmail.com>
2023-11-03 17:37:31 +0100

    initial commit

---
+++ src/main/java/com/pwee/eventmanagmentapp/EventManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.pwee.eventmanagmentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwee/eventmanagmentapp/EventManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwee.eventmanagmentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
